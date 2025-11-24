a251550c60a9383f79ef89bd0257bff9f75d472a
TornovDutta <tornovdutta@gmail.com>
2025-11-24 17:37:52 +0530

    add state

--- state/src/App.jsx
+++ state/src/App.jsx
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react'
+import { useState } from 'react'
 function App() {
+  const [count, setCount]=useState(0);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://react.dev"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={()=>
+        setCount(count+1)
+      }>{count}</button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
