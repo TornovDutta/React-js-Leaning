eccd1cb1bd560ff51c0e3e0dbede8fbb71d5d9ac
TornovDutta <tornovdutta@gmail.com>
2025-12-19 13:24:00 +0530

    add the state

--- stateandcompoenets/src/App.jsx
+++ stateandcompoenets/src/App.jsx
@@ -1,10 +1,61 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from 'react'
 
 function App() {
+  const [count,setCount]=useState(0);
+  const [arr,setArr]=useState([
+    {"id":1,"name":"Tornov"},
+    {"id":2,"name":"Ben"}
+  ])
+  const [name,setName]=useState("");
+  const [show,hide]=useState(true)
+
+  function handleAdd(){
+    setArr([
+    ...arr,
+    {
+      id: arr.length + 1,
+      name: name
+    }
+  ]);
+  setName('');
+  }
+  function handleSub(){
+    if(arr.length==0)return;
+    setArr(arr.slice(0,arr.length-1));
+  }
   return (
-    <div className="App"></div>
-  );
+    <div className="App">
+      {show && (
+        <div>
+          <p>{count}</p>
+          <button onClick={() => setCount(count + 1)}>Add</button>
+          <button onClick={() => setCount(count - 1)}>Sub</button>
+        </div>
+      )}
+
+      <hr />
+      {!show && (
+  <>
+    <ul>
+      {arr.map(element => (
+        <li key={element.id}>{element.name}</li>
+      ))}
+    </ul>
+
+    <input
+      type="text"
+      value={name}
+      onChange={(e) => setName(e.target.value)}
+    />
+
+    <button onClick={handleAdd}>Add</button>
+    <button onClick={handleSub}>Sub</button>
+  </>
+  )}
+  <button onClick={()=>hide(!show)}>change</button>
+
+    </div>
+  )
 }
 
-export default App;
+export default App
